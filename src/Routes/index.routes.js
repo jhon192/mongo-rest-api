a030ab7e7e01cd9b75b8183400665039a0be9e61
jhon192 <jhon19.2@outlook.com>
2022-02-15 01:23:24 -0400

    update index.routes

--- src/Routes/index.routes.js
+++ src/Routes/index.routes.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const { ObjectId, BSONType } = require("mongodb");
 const router = Router();
 
 const connect = require("../database");
@@ -6,6 +7,14 @@ const connect = require("../database");
 router.get("/", async (req, res) => {
   const db = await connect();
   const result = await db.collection("schedule").find({}).toArray();
+  res.json(result);
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const db = await connect();
+  const result = await db.collection("schedule").findOne({ _id: ObjectId(id) });
+
   res.json(result);
 });
 
@@ -18,6 +27,30 @@ router.post("/", async (req, res) => {
   };
 
   const result = db.collection("schedule").insertOne(schedule);
+  res.json(result);
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const db = await connect();
+  const schedule = {
+    nombre: req.body.nombre,
+    apellido: req.body.apellido,
+    telefono: req.body.telefono,
+  };
+  const result = db
+    .collection("schedule")
+    .updateOne({ _id: ObjectId(id) }, { $set: schedule });
+
+  res.json(result);
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const db = await connect();
+
+  const result = db.collection("schedule").deleteOne({ _id: ObjectId(id) });
+
   res.json(result);
 });
 
