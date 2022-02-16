38c94ec7d3272ec41c7a36f6ff883c209b6d02f1
jhon192 <jhon19.2@outlook.com>
2022-02-15 21:42:46 -0400

    updating database url

--- src/database.js
+++ src/database.js
@@ -2,13 +2,15 @@ const { MongoClient } = require("mongodb");
 
 const connect = async () => {
   try {
-    const client = await MongoClient.connect("mongodb://127.0.0.1:27017");
-    const db = client.db("node-restapi")
+    const client = await MongoClient.connect(
+      "mongodb+srv://jhon192:1234@cluster0.bvoxc.mongodb.net/test"
+    );
+    const db = client.db("node-restapi");
     console.log("connection was successfully");
     return db;
   } catch (e) {
     console.log(e);
   }
-}
+};
 
 module.exports = connect;
