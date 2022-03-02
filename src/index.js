3c0f511f0fe8fe6716bb75848f113d9e240633a9
jhon192 <jhon19.2@outlook.com>
2022-03-02 15:30:14 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-const ServerlessHttp = require("serverless-http");
+
 const app = require("./server");
 
-// const Main = async () => {
-//   await app.listen(app.get("port"));
-//   console.log("Run on port ", app.get("port"));
-//   return app;
-// };
+const Main = async () => {
+  await app.listen(app.get("port"));
+  console.log("Run on port ", app.get("port"));
+  return app;
+};
 
-// Main();
+Main();
 
 
-module.exports.handler = ServerlessHttp(app)
