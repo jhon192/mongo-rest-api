ca1765390608dca03f94f097228f0b34bd36c538
jhon192 <jhon19.2@outlook.com>
2022-03-02 15:32:36 -0400

    update

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = require("./Routes/index.routes");
+const cors = require('cors');
 
 const app = express();
 
 app.set("port", process.env.PORT || 4000)
+
+app.use(
+    cors({
+        credentials: true,
+        origin: true
+    })
+);
+app.options('*', cors());
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
