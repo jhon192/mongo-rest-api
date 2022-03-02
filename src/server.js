3ff4b1b11bb9aba6e95dd88847c45560176a682d
jhon192 <jhon19.2@outlook.com>
2022-03-02 15:22:02 -0400

    update

--- src/server.js
+++ src/server.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const router = require("./Routes/index.routes");
 
-const IndexRoute = require("./Routes/index.routes")
-
 const app = express();
 
 app.set("port", process.env.PORT || 4000)
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-app.use(IndexRoute)
+app.use(router)
 
 module.exports = app;
